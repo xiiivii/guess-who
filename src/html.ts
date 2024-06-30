5a4571f54e0d35abb13fcac065c81973f4fcd176
Oleg Semichev <dallassCamelCase@gmail.com>
2024-06-30 14:45:13 +0400

    Update html.ts

--- src/html.ts
+++ src/html.ts
@@ -2,6 +2,7 @@ export const html = `
 <html lang="en">
   <head>
     <meta charset="UTF-8" />
+    <meta http-equiv="refresh" content="600" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Guess game</title>
   </head>
